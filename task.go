516afcd12c8d6cca28462066f31682aea2c68cc3
ryan-wagiu <wagiuryan@gmail.com>
2022-08-26 19:55:31 +0800

    coba1

--- task.go
+++ task.go
@@ -18,7 +18,10 @@ import "fmt"
 func main() {
   fmt.println("hello world")
   fmt.println("hello world2")
+  fmt.println("hello world2")
+  fmt.println("hello world2")
   fmt.println("Develop")
 
 }
 >>>>>>> fitur2
+
