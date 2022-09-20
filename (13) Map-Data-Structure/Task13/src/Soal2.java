3efde770e95e092a30784563cec9c9183619997e
Ryan-wagiu <79500145+Ryan-wagiu@users.noreply.github.com>
2022-09-20 18:58:52 +0800

    Update Soal2.java

--- (13) Map-Data-Structure/Task13/src/Soal2.java
+++ (13) Map-Data-Structure/Task13/src/Soal2.java
@@ -1,2 +1,9 @@
 public class Soal2 {
+  public static void main(String[] args) {
+        String angka = "78237236";
+
+        for (int i = 0; i < angka.length(); i ++){
+            System.out.println(angka.charAt(i));
+        }
+  }
 }
