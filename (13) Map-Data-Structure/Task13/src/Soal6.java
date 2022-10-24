1100259a8faf241b744f9ecbcdb2dd958c4c13d8
Ryan-wagiu <79500145+Ryan-wagiu@users.noreply.github.com>
2022-10-24 15:11:17 +0800

    Update Soal6.java

--- (13) Map-Data-Structure/Task13/src/Soal6.java
+++ (13) Map-Data-Structure/Task13/src/Soal6.java
@@ -1,2 +1,26 @@
+import java.util.Arrays;
+
 public class Soal6 {
+    public static void main(String[] args) {
+        Integer[] input = {2, 1, 5, 1, 3, 2};
+        int k = 3;
+
+        int result = 0;
+        for (int i = 0; i < input.length; i++) {
+            Integer[] inputSlice = Arrays.copyOfRange(input, i, i + k);
+
+            int total = 0;
+            for (int j = 0; j < inputSlice.length; j++) {
+                if (inputSlice[j] != null) {
+                    total += inputSlice[j];
+                }
+            }
+
+            if (total > result) {
+                result = total;
+            }
+        }
+
+        System.out.println("Output : " + result);
+    }
 }
